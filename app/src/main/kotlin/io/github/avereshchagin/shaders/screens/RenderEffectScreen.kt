5951ef6c2db28f7bf73426c10988203bd7e1f367
Aleksandr Vereshchagin <a.vereshchagin@vk.team>
2025-09-03 16:11:57 +0300

    Workshop updates - Render effect

--- app/src/main/kotlin/io/github/avereshchagin/shaders/screens/RenderEffectScreen.kt
+++ app/src/main/kotlin/io/github/avereshchagin/shaders/screens/RenderEffectScreen.kt
@@ -2,10 +2,6 @@ package io.github.avereshchagin.shaders.screens
 
 import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.rememberTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -19,61 +15,30 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.FloatState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import org.intellij.lang.annotations.Language
-
-@Language("AGSL")
-const val Source = """
-    uniform shader texture;
-    
-    uniform float2 iResolution;
-    uniform float stripesCount;
-    
-    float GLASS_REFRACTION = 0.125;
-    
-    float2 glass(float2 uv) {
-        float xShift = fract((uv.x - 0.5) * stripesCount + 0.5) - 0.5;
-        uv.x += xShift * GLASS_REFRACTION;
-        return uv;
-    }
-
-    half4 main(float2 cord) {
-        float2 uv = cord / iResolution.xy;
-        uv = glass(uv);
-        half4 color = texture.eval(uv * iResolution.xy);
-        return color;
-    }
-"""
-
-@Language("AGSL")
-const val Source2 = """
-    uniform shader texture;
-    
-    uniform float2 iResolution;
-    uniform float iTime;
-    
-    half4 main(float2 cord) {
-        float2 uv = cord / iResolution.xy;
-        uv.y = uv.y + cos(uv.x * 3.14 * 4.0) * iTime;
-        half4 color = texture.eval(uv * iResolution.xy);
-        return color;
-    }
-"""
 
 @Language("AGSL")
 const val LensDistortion = """
     uniform shader texture;
     
     uniform float2 iResolution;
-    uniform float iTime;
+    uniform float iAlpha;
+    layout(color) uniform half4 iBgColor;
     
     vec2 lens_distortion(vec2 r, float a) {
         return r * (1.0 - a * dot(r, r));   
@@ -87,13 +52,13 @@ const val LensDistortion = """
         float2 uv = cord / iResolution.xy;
         float2 center = float2(0.5, 0.5);
         
-        vec2 distortion = lens_distortion(uv - center, -2.5);
+        vec2 distortion = lens_distortion(uv - center, iAlpha);
         vec2 zoomed = zoom_point(uv + distortion, center, 2.0);
         
         if (zoomed.x > 0.0 && zoomed.y > 0.0 && zoomed.x < 1.0 && zoomed.y < 1.0) {
             return texture.eval(zoomed * iResolution.xy);
         } else {
-            return half4(1.0);
+            return iBgColor;
         }
     }
 """
@@ -101,18 +66,20 @@ const val LensDistortion = """
 @Composable
 private fun ShaderContainer(
     modifier: Modifier = Modifier,
-    stripes: State<Float>,
+    sliderPosition: FloatState,
     content: @Composable BoxScope.() -> Unit,
 ) {
     val runtimeShader = remember {
         RuntimeShader(LensDistortion)
     }
+    val bgColor = MaterialTheme.colorScheme.background
     Box(
         modifier = modifier
+            .clipToBounds()
             .graphicsLayer {
                 runtimeShader.setFloatUniform("iResolution", size.width, size.height)
-//                runtimeShader.setFloatUniform("stripesCount", stripes.value)
-                runtimeShader.setFloatUniform("iTime", stripes.value)
+                runtimeShader.setFloatUniform("iAlpha", sliderPosition.floatValue)
+                runtimeShader.setColorUniform("iBgColor", bgColor.toArgb())
                 renderEffect = RenderEffect
                     .createRuntimeShaderEffect(
                         runtimeShader, "texture"
@@ -126,22 +93,18 @@ private fun ShaderContainer(
 @Composable
 fun RenderEffectScreen() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            val stripes = remember {
-                MutableTransitionState(0.0f)
+            val sliderPositionState = remember {
+                mutableFloatStateOf(0f)
             }
-            val transition = rememberTransition(stripes)
-            val s = transition.animateFloat(
-                transitionSpec = {
-                    tween(durationMillis = 2000)
-                }
-            ) { it }
+            var sliderPosition by sliderPositionState
+
             ShaderContainer(
-                stripes = s
+                sliderPosition = sliderPositionState
             ) {
                 Column(
                     modifier = Modifier.padding(16.dp),
@@ -165,7 +128,6 @@ fun RenderEffectScreen() {
                         Button(
                             modifier = Modifier.weight(1f),
                             onClick = {
-//                                stripes.targetState = 0.03f
                             }
                         ) {
                             Text("OK")
@@ -174,7 +136,6 @@ fun RenderEffectScreen() {
                         Button(
                             modifier = Modifier.weight(1f),
                             onClick = {
-//                                stripes.targetState = 0f
                             }
                         ) {
                             Text("Cancel")
@@ -182,6 +143,13 @@ fun RenderEffectScreen() {
                     }
                 }
             }
+
+            Slider(
+                modifier = Modifier.padding(16.dp),
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = -4f .. 4f,
+            )
         }
     }
 }
